918b0338a93a2bd3b776668ebf34539a71880956
Mary Madonna <mwiafeansong@gmail.com>
2022-04-08 15:59:13 -0700

    two digit combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 9; i++)
 	{
-	for (j = i + 1; j < 9; j++)
+	for (j = i + 1; j < 10; j++)
 	{
 	putchar((i % 10) + '0');
 	putchar((j % 10) + '0');
