6dc74aa3a1db75c2b66f35ed1b48a60f89ee714d
Mary Madonna <mwiafeansong@gmail.com>
2022-04-12 09:30:31 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value
+ * @n - value to be checked
+ *
+ * Return: n if not zero, 0 if zero
+ */
+
+int _abs(int n)
+{
+	if (n > 0 || n < 0)
+		return (n);
+	else
+		return (0);
+}
