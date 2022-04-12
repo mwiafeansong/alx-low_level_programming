7ded66d04ae00ffd8a43eff4868fc23ebae69c57
Mary Madonna <mwiafeansong@gmail.com>
2022-04-12 15:05:18 -0700

    multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			printf("%d, ", i)
+			sum += i;
+		}
+	}
+	printf("sum = %d\n", sum);
+}
