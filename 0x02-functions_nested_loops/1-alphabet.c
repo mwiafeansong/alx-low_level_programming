a14f08c6dd03564c5f9d9de1dbd545abbeb8e300
Mary Madonna <mwiafeansong@gmail.com>
2022-04-12 06:09:29 -0700

    print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ *
+ * Return: None
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
