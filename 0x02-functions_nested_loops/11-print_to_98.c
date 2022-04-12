46cba20c8d6d3599f0364fe72a0b84119a70822c
Mary Madonna <mwiafeansong@gmail.com>
2022-04-12 13:43:29 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers to 98
+ * @n: starting number
+ *
+ * Return: None
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", 98);
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", 98);
+	}
+}
