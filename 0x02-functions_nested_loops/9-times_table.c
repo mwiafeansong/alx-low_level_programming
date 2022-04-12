7627eb67776d876a89773df369f8f94991611289
Mary Madonna <mwiafeansong@gmail.com>
2022-04-12 14:20:28 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ *
+ * Return: None
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i =0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+
+			if ((k / 10) == 0)
+			{
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar((k % 10) + '0');
+				}
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
