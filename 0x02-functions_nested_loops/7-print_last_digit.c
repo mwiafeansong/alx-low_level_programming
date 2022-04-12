91a8c1956bb24ca565202107eb2bec37e7695267
Mary Madonna <mwiafeansong@gmail.com>
2022-04-12 13:08:24 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: number whose last digit is to be printed
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = (n % 10) * -1;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+}
