905527fc6d2675eeab2afaa6748410f179ac0e0a
Mary Madonna <mwiafeansong@gmail.com>
2022-04-12 21:29:05 -0700

    first 50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 2;
+	int fib;
+	int i;
+
+	printf("%d, ", num1);
+	printf("%d, ", num2;
+	for (i = 0; i < 48; i++)
+	{
+		fib = num1 + num2;
+		printf("%d", fib);
+		if (i == 47)
+			break;
+		printf(", ");
+		num1 = num2;
+		num2 = fib;
+	}
+	printf("\n");
+
+	return (0);
+}
