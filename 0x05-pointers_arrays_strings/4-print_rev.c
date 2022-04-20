52fc6e89502084725672e5b18c3d2ff6e5823bf7
Mary Madonna <mwiafeansong@gmail.com>
2022-04-20 08:18:56 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ * Return: None
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = len + 1;
+	}
+
+	for (j = len; j >= 0; j++)
+	{
+		_putchar(s[j]);
+	}
+}
