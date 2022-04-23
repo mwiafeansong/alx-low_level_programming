ffb32a0028291a2e87469fb4705993cd67b175c4
Mary Madonna <mwiafeansong@gmail.com>
2022-04-23 03:59:33 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char replacee[] = {65, 69, 79, 84, 76, 97, 101, 111, 116, 108};
+	char replacer[] = {52, 51, 48, 55, 49, 52, 51, 48, 55, 49};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == replacee[j])
+				s[i] = replacer[j];
+		}
+	}
+}
