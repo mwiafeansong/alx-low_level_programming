4f731cf8f0db2b6aa4cc1ef1ddd394856c4696c4
Mary Madonna <mwiafeansong@gmail.com>
2022-04-28 14:18:03 -0700

    is prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * iteration - function to loop through
+ * @n: integer in question
+ * @i: iterator
+ *
+ * Return: 0 or 1
+ */
+
+int iteration(int n, int i)
+{
+	if (i == n - 1)
+		return (1);
+	if (n % i == 0 || n < 2)
+		return (0);
+	return(iteration(n, i + 1));
+}
+
+/**
+ * is_prime_number - checks if an integer is a prime number
+ * @n: the integer
+ *
+ * Return: 0 if false, 1 if true
+ */
+
+int is_prime_number(int n)
+{
+	int i = 2;
+	return (iteration(n, i));
+}
