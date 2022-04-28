38951c88a81260f6f671d20f68b1b519fab58f83
Mary Madonna <mwiafeansong@gmail.com>
2022-04-28 12:04:30 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - finds factorial of a number
+ * @n: the number
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
