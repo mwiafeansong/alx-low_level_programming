317c7a1dfeed14d46ade9aef06fed0fd75a709ea
Mary Madonna <mwiafeansong@gmail.com>
2022-04-28 10:50:09 -0700

    print reverse of a string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: the string
+ *
+ * Return: None
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
