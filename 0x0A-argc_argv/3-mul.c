a97797e51da81c103145c0453d66bb96c372d138
Mary Madonna <mwiafeansong@gmail.com>
2022-05-04 09:39:42 -0700

    multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: command line arguments
+ *
+ * Return: 0 if success, 1 if failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
+
+	return (0);
+}
+
