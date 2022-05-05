a79dff0523195c94bfe7a248199a6a81535ad09f
Mary Madonna <mwiafeansong@gmail.com>
2022-05-05 11:08:26 -0700

    create 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = (int **)malloc(height * sizeof(int));
+	p = (int **)malloc(height * sizeof(int *));
 
 	if (p == NULL)
 	{
