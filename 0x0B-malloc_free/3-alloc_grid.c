cbf96a04600189b4743af3721ea221c0ab89c3ee
Mary Madonna <mwiafeansong@gmail.com>
2022-05-05 11:00:11 -0700

    create 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,21 +6,18 @@
  * @width: number of columns
  * @height: number of rows
  *
- * Return: pointer to 2D array of integers or NULL
+ * Return: pointer to 2D array or NULL
  */
 
 int **alloc_grid(int width, int height)
 {
-	int a, i, j;
+	int i, j;
 	int **p;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	p = (int **)malloc(height * sizeof(int));
-
-	for (a = 0; a < height; a++)
-		p[a] = (int *)malloc(width * sizeof(int));
 
 	if (p == NULL)
 	{
@@ -30,8 +27,21 @@ int **alloc_grid(int width, int height)
 	{
 		for (i = 0; i < height; i++)
 		{
-			for (j = 0; j < width; j++)
-				p[i][j] = 0;
+			p[i] = (int *)malloc(width * sizeof(int));
+
+			if (p[i] == NULL)
+			{
+				while (--i)
+					free(p[i]);
+				free(p[0]);
+				free(p);
+				return (NULL);
+			}
+			else
+			{
+				for (j = 0; j < width; j++)
+					p[i][j] = 0;
+			}
 		}
 		return (p);
 	}
