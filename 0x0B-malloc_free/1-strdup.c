44da7a4ce1699a5f514292be8f7d001c1057cf35
Mary Madonna <mwiafeansong@gmail.com>
2022-05-05 06:23:49 -0700

    copy a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,13 @@
 char *_strdup(char *str)
 {
 	int i, j;
+	char *p;
 	int len = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
 
-	char *p = malloc((len + 1) * sizeof(char));
+	p = malloc((len + 1) * sizeof(char));
 
 	if (str == NULL || p == NULL)
 	{
