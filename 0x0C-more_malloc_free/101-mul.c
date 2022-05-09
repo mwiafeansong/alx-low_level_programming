bae9124663dd3bf87b8616bd9ceb5dd5251ee120
Mary Madonna <mwiafeansong@gmail.com>
2022-05-09 13:06:27 -0700

    multiply integers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -53,7 +53,7 @@ void print_int(unsigned long int n)
 {
 	unsigned long int i, resp, divisor = 1;
 
-	for (i = 0; n / divisor > 9; i++)
+	for (i = 0; (n / divisor) > 9; i++)
 		divisor *= 10;
 
 	for (; divisor >= 1; n %= divisor, divisor /= 10)
@@ -77,7 +77,7 @@ int main(int argc, char const *argv[])
 
 	if (argc != 3)
 	{
-		_puts("Error ");
+		_puts("Error\n");
 		exit(98);
 	}
 	print_int(_atoi(argv[1]) * _atoi(argv[2]));
