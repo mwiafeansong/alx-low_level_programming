372d89539485fadc7a0591c24dfe4bce10ce4c8e
Mary Madonna <mwiafeansong@gmail.com>
2022-05-09 11:32:12 -0700

    memory for array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb *size);
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
