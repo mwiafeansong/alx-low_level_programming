57c7c1d2683dc9a1ab1cd05c15e975406e1d5dde
Mary Madonna <mwiafeansong@gmail.com>
2022-05-10 01:26:38 -0700

    multiply integers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,89 +1,155 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdio.h>
 
 /**
- * _puts - prints a string, then a new line
- * @str: string to print
- *
- * Return: none
+ * _strlen - finds length of a string
+ * @s: string
+ * 
+ * Return: length
  */
 
-void _puts(char *str)
+int _strlen(char *s)
+{
+	unsigned int i, n = 0;
+	
+	for (i = 0; s[i] != '\0'; i++)
+		n++;
+	return (n);
+}
+
+/**
+ * populateResult - multiplies two numbers in string
+ * @dest: product of s1 and s2 
+ * @s1: first number in string format
+ * @s2: second number in string format
+ * @len_s1: length of s1
+ * @len_s2 : length of s2
+ * 
+ * Return: None
+ */
+
+void populateResult(char *dest, char *s1, char *s2, int len_s1, int len_s2)
+{
+	int i, j, k, temp, non_carry_value;
+	int carry_value = 0;
+	char *multip;
+	char *multiplier;
+	
+	if (len_s1 > len_s2)
+	{
+		i = len_s1 - 1;
+		j = len_s2 - 1;
+		multip = s1;
+		multiplier = s2;
+	}
+	else
+	{
+		i = len_s2 - 1;
+		j = len_s1 - 1;
+		multip = s2;
+		multiplier = s1;
+	}
+	
+	while (i >= 0)
+	{
+		k = i;
+		
+		while (k >= 0)
+		{
+			temp = ((nultip[k] - '0') * (multiplier[j] - '0'));
+			temp += carry_value;
+			if (j + 1 <= len_s2 - 1 && dest[k + j + 1] >= '0' && dest[k + j + 1] <= '9')
+				temp += dest[k + j + 1] - '0';
+			if (temp < 10)
+			{
+				non_carry_value = temp;
+				carry_value = 0;
+			}
+			else
+			{
+				non_carry_value = temp % 10;
+				carry_value = temp / 10;
+			}
+			dest[k + j + 1] = non_carry_value + '0';
+			k--;
+		}
+		if (carry_value)
+			dest[k + j + 1] = carry_value + '0';
+		
+		carry_value = 0;
+		
+		if (j > 0)
+			j--;
+		else
+			i = -1;
+	}
+	free(dest);
+	free(multip);
+	free(multiplier);
+}
+
+/**
+ * print_result - prints numbers
+ * @s: number in string format 
+ * @n: length of s
+ * 
+ * Return: None
+ */
+
+void print_result(char *s, int n)
 {
 	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-}
-
-/**
- * _atoi - converts ascii to integer
- * @s: the string
- *
- * Return: integer
- */
-
-int _atoi(const char *s)
-{
-	int sign = 1;
-	unsigned long int first_num, i, resp = 0;
-
-	for (first_num = 0; !(s[first_num] >= 48 && s[first_num] <= 57); first_num++)
+	
+	for (i = 0; i < n; i++)
 	{
-		if (s[first_num] == '-')
-			sign *= -1;
-	}
-
-	for (i = first_num; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-
-	return (sign * resp);
-}
-
-/**
- * print_int - prints an integer
- * @n: integer
- *
- * Return : None
- */
-
-void print_int(unsigned long int n)
-{
-	unsigned long int i, resp, divisor = 1;
-
-	for (i = 0; (n / divisor) > 9; i++)
-		divisor *= 10;
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar(resp + '0');
+		if (s[i] >= '0' && s[i] <= '9')
+			printf("%c", s[i]);
 	}
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of command line arguments
- * @argv: command line arguments
- *
- * Return: 0 if success
+ * main - Entry point
+ * @argc: number of command line arguments 
+ * @argv: the arguments
+ * 
+ * Return: 0 if success 
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
-
+	int len1, len2;
+	char *result;
+	
 	if (argc != 3)
 	{
-		_puts("Error\n");
+		printf("Error\n");
 		exit(98);
 	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
-	_putchar('\n');
-
+	
+	len1 = _strlen(argv[1]);
+	if (!len1)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	len2 = _strlen(argv[2]);
+	if (!len2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	result = malloc(len1 + len2);
+	if (result == NULL)
+		return (1);
+	populateResult(result, argv[1], argv[2], len1, len2);
+	print_result(result, len1 + len2);
+	printf("\n");
+	free(result);
+	
 	return (0);
 }
-
---
+++ 0x0C-more_malloc_free/101-mula.c
@@ -0,0 +1,89 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _puts - prints a string, then a new line
+ * @str: string to print
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
+
+/**
+ * _atoi - converts ascii to integer
+ * @s: the string
+ *
+ * Return: integer
+ */
+
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int first_num, i, resp = 0;
+
+	for (first_num = 0; !(s[first_num] >= 48 && s[first_num] <= 57); first_num++)
+	{
+		if (s[first_num] == '-')
+			sign *= -1;
+	}
+
+	for (i = first_num; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+ * print_int - prints an integer
+ * @n: integer
+ *
+ * Return : None
+ */
+
+void print_int(unsigned long int n)
+{
+	unsigned long int i, resp, divisor = 1;
+
+	for (i = 0; (n / divisor) > 9; i++)
+		divisor *= 10;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar(resp + '0');
+	}
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of command line arguments
+ * @argv: command line arguments
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_puts("Error\n");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
+
