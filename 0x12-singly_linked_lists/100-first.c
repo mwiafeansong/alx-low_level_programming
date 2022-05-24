83848c6ac9d63de538ce2f4ef19237fe791efe80
Mary Madonna <mwiafeansong@gmail.com>
2022-05-24 12:15:46 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * before_main - executes before main
+ *
+ * Return: None
+ */
+
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
