29d1ad44d3f7feb7a1e3823e6dccf33d87513cda
Mary Madonna <mwiafeansong@gmail.com>
2022-05-26 10:33:51 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @h: singly linked list
+ *
+ * Return: None
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr = head;
+
+	while (ptr != NULL)
+	{
+		head = head->next;
+		free(ptr);
+		ptr = head;
+	}
+}
