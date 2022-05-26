c3dbb9a56d5ff1e01981a90aaedf441fb50544e6
Mary Madonna <mwiafeansong@gmail.com>
2022-05-26 15:38:13 -0700

    delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node at index specified
+ * @head: pointer to singly linked list
+ * @index: position of node to delete
+ *
+ * Return: 1 (Success) or -1 (Failure)
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *ptr = *head;
+	listint_t *ptr2 = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		ptr = ptr->next;
+		free(ptr2);
+		*head = ptr;
+	}
+	else
+	{
+		for (i = 0; i < index - 1; i++)
+		{
+			if (ptr == NULL)
+				return (-1);
+			ptr = ptr->next;
+		}
+		ptr2 = ptr->next;
+		ptr->next = ptr2->next;
+		free(ptr2);
+	}
+
+	return (1);
+}
