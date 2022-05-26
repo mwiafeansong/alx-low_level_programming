d2602bd3481d8faa927e7c03881cbd929f7976fc
Mary Madonna <mwiafeansong@gmail.com>
2022-05-26 10:04:41 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a singly linked list
+ * @h: singly linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
