7985e1c362009f7cbbe9eb0a0bd31637a3fd8aa6
Mary Madonna <mwiafeansong@gmail.com>
2022-06-01 23:17:26 -0700

    binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary number to unsigned int
+ * @b: binary number in string format
+ *
+ * Return: converted number or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, total = 0, dec_value = 1;
+	unsigned int j;
+
+	if (!b)
+		return (0);
+
+	for (j = 0; b[j] != '\0'; j++)
+		;
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		if (b[i] != 0 && b[i] != 1)
+			return (0);
+		if (b[i] == 1)
+		{
+			total += dec_value;
+			dec_value *= 2;
+		}
+	}
+
+	return (total);
+}
