3bafe78ddb962809eacbab6cee7833518a996b06
Mary Madonna <mwiafeansong@gmail.com>
2022-06-02 10:26:54 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - finds number of bits needed to be flipped
+ * to get from one number ot another
+ * @n: first number
+ * @m: second number
+ *
+ * Return : number of bits needed to be flipped
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int num_bits;
+
+	for (num_bits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			num_bits++;
+	}
+
+	return (num_bits);
+}
