327476f2acf772fc28980686d10290e64eb7712c
Mary Madonna <mwiafeansong@gmail.com>
2022-06-02 00:26:16 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = j - 1; i >= 0; i--)
 	{
-		if (b[i] < '0' && b[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		if (b[i] == '1')
 			total += dec_value;
