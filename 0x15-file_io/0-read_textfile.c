0d97ba95350c7123fb2c66007a7a64968ff11823
mwiafeansong <mwiafeansong@gmail.com>
2022-06-21 13:08:55 +0000

    read and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints to standard output
+ * @filename: file to read from
+ * @letters: number of letters to read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nread, nwrite;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters * sizeof(char));
+	if (!buf)
+		return (0);
+
+	nread = read(fd, buf, letters);
+	nwrite = write(STDOUT_FILENO, buf, letters);
+
+	close(fd);
+	free(buf);
+
+	return (nwrite);
+}
